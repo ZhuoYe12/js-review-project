d4e99d0ee5d2b09e1a7e8295d0bf463af592f017
ZhuoYe12 <christiandhave3@gmail.com.com>
2025-01-31 08:22:49 +0800

    Added Todo CRUD Management project

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// === Todo CRUD Management ====
-
 // Array to store todos
 let todos = [];
 
 // DOM Elements
-const todoForm = document.getElementById('todo-form' );
+const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
 // Function to render todos
-function renderTodos ( ) {
+function renderTodos() {
     todoList.innerHTML = ''; // Clear the list
     todos.forEach((todo, index) => {
         const li = document.createElement('li');
         li.className = 'todo-item';
         li.innerHTML = `
-            <span>${todo}</span><>
+            <span>${todo}</span>
             <button onclick="editTodo(${index})">Edit</button>
-            <button onclick="deleteTodo(${index})">Delete</button></>
+            <button onclick="deleteTodo(${index})">Delete</button>
         `;
         todoList.appendChild(li);
     });
@@ -25,34 +23,34 @@ function renderTodos ( ) {
 
 // Function to add a new todo
 function addTodo(event) {
-    event.preventDefault(); // Prevent form submission 
+    event.preventDefault(); // Prevent form submission
     const newTodo = todoInput.value.trim();
     if (newTodo) {
         todos.push(newTodo);
-        todoInput.value = ''; // Clear the input 
+        todoInput.value = ''; // Clear the input
         renderTodos();
     }
 }
 
-    // Function to edit a todo
-    function editTodo (index) {
-        const updatedTodo = prompt ('Edit your todo:', todos [index]); 
-        if (updatedTodo !== null) {
-            todos[index] = updatedTodo.trim();
-            renderTodos();
+// Function to edit a todo
+function editTodo(index) {
+    const updatedTodo = prompt('Edit your todo:', todos[index]);
+    if (updatedTodo !== null) {
+        todos[index] = updatedTodo.trim();
+        renderTodos();
     }
 }
 
-    // Function to delete a todo
-    function deleteTodo (index) {
-        if (confirm('Are you sure you want to delete this todo?')) { 
-            todos.splice (index, 1);
-            renderTodos();
+// Function to delete a todo
+function deleteTodo(index) {
+    if (confirm('Are you sure you want to delete this todo?')) {
+        todos.splice(index, 1);
+        renderTodos();
     }
 }
 
-//even listeners
-todoForm.addEventListener('Sumbit', addTodo);
+// Event listener for form submit
+todoForm.addEventListener('submit', addTodo);
 
-//Initial render 
+// Initial render
 renderTodos();
